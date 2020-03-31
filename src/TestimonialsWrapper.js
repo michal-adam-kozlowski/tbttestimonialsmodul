5c5a7787d5944b0151a6c20a1759d4b40f9db61e
Michał Kozłowski <michalkozlowski@michal-kozlowski-macbook-pro.home>
2020-03-31 17:34:33 +0200

    randomizacja i unikalne

--- src/TestimonialsWrapper.js
+++ src/TestimonialsWrapper.js
@@ -3,15 +3,17 @@ import Testimonial from "./Testimonial.js";
 import "./TestimonialsWrapper.scss";
 
 
-var Testimonials_Arr = [
+var TestimonialsArr = [
     {
+        index : 1,
         image : "./img/girl1.jpg",
         name : 'Ania',
         rating : 5,
         testimonial_text : 'Najlepsze wyjazdy, cudowni ludzie, niepowtarzalne wspomnienia!'
     },
 
-        {
+    {
+        index : 2,
         image : './img/man1.jpg',
         name : 'Jacek',
         rating : 4,
@@ -19,19 +21,55 @@ var Testimonials_Arr = [
         },
 
     {
+        index : 3,
         image : './img/man2.jpg',
         name : 'Robert',
         rating : 3,
         testimonial_text : 'Autokar smierdzial i dlatego 3!'
+        },
+    {
+        index : 4,
+        image : './img/man2.jpg',
+        name : 'Marian',
+        rating : 5,
+        testimonial_text : 'Zajebongo!'
+        },
+    {
+        index : 5,
+        image : './img/man1.jpg',
+        name : 'Januszo',
+        rating : 2,
+        testimonial_text : 'Najgorsze biuro EVER!'
         }
     ];
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+function GenerateTestimonialsArr() {
+    var arr = [];
+    var arr2 = [];
+    while(arr.length < 3){
+        var r = getRandomInt(1, 5);
+        if(arr.indexOf(r) === -1) {
+            arr.push(r);
+            TestimonialsArr.forEach((el) => {
+                if (el.index === r) {
+                    arr2.push(el)
+                }
+            })
+        }
+    }
+    return arr2
+}
 
 class TestimonialsWrapper extends Component {
     render () {
         return (
             <div className='Testimonials_Wrapper'>
-                    {Testimonials_Arr.map((el) =>
+                    {GenerateTestimonialsArr().map((el) =>
                         <Testimonial image={el.image} name={el.name} rating={el.rating} text={el.testimonial_text}/>
                     )}
             </div>
